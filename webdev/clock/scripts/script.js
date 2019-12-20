c4f48138068241e376dda52b4b7840a255a4a17d
lucas <lucas.tx.reis@gmail.com>
2019-12-20 19:33:41 -0200

    Responsivness and HALL added

--- webdev/clock/scripts/script.js
+++ webdev/clock/scripts/script.js
@@ -1,3 +1,16 @@
+var midday = "am";
+
+function greetings (hour) {
+
+    var tmp = "";
+    if(hour < 12) tmp = "morning,";
+    else if(hour < 18) tmp = "afternoon,";
+    else tmp = "evening,";
+
+    var msg = document.getElementById('greeting');
+    msg.innerText = "Good " + tmp + " Dave.";
+}
+
 
 var showTime = function() {
 
@@ -8,8 +21,16 @@ var showTime = function() {
     var minute = currentTime.getMinutes();
     var second = currentTime.getSeconds();
 
-    var time = hour + ':' + minute + ':' + second;
+    // Leading zeroes in any time that is lesser than 10 units
+    if(minute+1 <= 10) minute = "0" + minute.toString(10);
+    if(second+1 <= 10) second = "0" + second.toString(10);
+
+    if(hour >= 12) midday = "pm";
+    
+    greetings(hour);
+    var time = hour + ':' + minute + ':' + second + " " + midday;
     clock.innerText = time;
+
 
 };
 
